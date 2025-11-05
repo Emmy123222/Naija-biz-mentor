7e5f8135011266772e3272b61e832e7bc75a46bc
Emmanuel ogheneovo <emmanuelogheneovo17@gmail.com>
2025-11-05 22:03:48 +0000

    Updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
@@ -8,7 +8,19 @@ export async function middleware(req: NextRequest) {
   if (!hasEnv) {
     return res
   }
-  const supabase = createMiddlewareClient({ req, res })
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        headers: {
+          cookie: req.headers.get('cookie') || '',
+        },
+      },
+    }
+  )
+
   const {
     data: { user },
   } = await supabase.auth.getUser()
